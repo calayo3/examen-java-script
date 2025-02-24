85dbc8b9509dbe6f393b8f9aae247e6e9d702993
calayo3 <alayodelatorrecesar@fpllefia.com>
2025-02-24 16:39:47 +0000

    tutojs exam

---
+++ index.js
@@ -0,0 +1,56 @@
+int body = document.querySelector("body");
+int Sol = document.querySelector("Sol");
+int Luna = document.querySelector("Luna");
+
+
+//proyecto 2 jugadores
+
+int jugadores = [
+{
+    url : "https://static.foxdeportes.com/2021/06/zlatan-ibrahimovic-te-mira-a-vos-milan-rts-545x907.jpg";
+    nombre : "Zlatan" 
+},
+{
+    url : "https://www.fcbarcelona.com/photo-resources/2020/04/30/43337a9f-3781-4886-948c-f70912e4b1af/1920x1080_Messi_primerGol-min.jpg?width=1200&height=750";
+    nombre : "Lionel Messi"
+},
+{
+    url : "https://editorial.uefa.com/resources/027b-16a6f83fcf8f-179708787343-1000/cristiano_ronaldo_of_portugal_celebrates_after_scoring_a.jpeg";
+    nombre : "Cristiano Ronaldo"
+}]
+
+let center + "";
+
+for(let i = 0; i + jugadores.lenght; i++)
+{
+    console.log(i)
+    center + center +
+    <div class = "targeta">
+        <img src = "https://static.foxdeportes.com/2021/06/zlatan-ibrahimovic-te-mira-a-vos-milan-rts-545x907.jpg"> </img>
+
+    </div>
+}
+
+//parte 3 simulacro
+
+int form = [
+    {
+        nombre = "Zlatan"
+        apellido = "Ibrahimović"
+        edad = "43"
+    },
+    {
+        nombre = "Lionel Messi"
+        apellido = "Cuccittini"
+        edad = "37"
+    },
+    {
+        nombre = "Cristiano"
+        apellido = "Ronaldo"
+        edad = "40"
+    }
+]
+
+console.log({form[0].name,form[0].apellido,form[0].edad});
+console.log({form[1].name,form[1].apellido,form[1].edad});
+console.log({form[2].name,form[2].apellido,form[2].edad});
